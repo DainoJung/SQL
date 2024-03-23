198a53b657e47e523e0618d2ffaff02e26d7f728
DainJung <wjdekdls3693@gmail.com>
2024-03-23 11:50:09 +0900

    CASE 표현식

--- 누구나 쉽게 SQL/hr.sql
+++ 누구나 쉽게 SQL/hr.sql
@@ -1,4 +1,9 @@
-SELECT *
-    FROM subway_statistics
-    WHERE station_name LIKE '선릉%'
-    ORDER BY 1 DESC, 2, 3, 4, 5, 6 ;
+SELECT emp_name, age
+       ,CASE WHEN age BETWEEN 0 AND 19 THEN '10대'
+             WHEN age BETWEEN 20 AND 29 THEN '20대'
+             WHEN age BETWEEN 30 AND 39 THEN '30대'
+             WHEN age BETWEEN 40 AND 49 THEN '40대'
+             WHEN age BETWEEN 50 AND 59 THEN '50대'
+             ELSE '60대 이상'
+        END ages
+FROM EMP03;
