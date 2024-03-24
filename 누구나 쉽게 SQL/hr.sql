d4492cf8da6d783d2b936b65181a97870e51f8eb
DainJung <wjdekdls3693@gmail.com>
2024-03-24 14:12:28 +0900

    집계함수

--- 누구나 쉽게 SQL/hr.sql
+++ 누구나 쉽게 SQL/hr.sql
@@ -1,5 +1,6 @@
-SELECT station_name
-    FROM subway_statistics
-WHERE gubun = '승차'
-GROUP BY station_name
-ORDER BY station_name;
+SELECT COUNT(*) cnt
+    ,MIN(passenger_number) min_value
+    ,MAX(passenger_number) max_value
+    ,SUM(passenger_number) sum_value
+    ,AVG(passenger_number) avg_value
+FROM subway_statistics;
