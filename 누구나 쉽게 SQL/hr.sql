2b9fde457df1722a5fe11bf4d63639a67717ace8
DainJung <wjdekdls3693@gmail.com>
2024-03-24 14:09:17 +0900

    GROUP BY 절

--- 누구나 쉽게 SQL/hr.sql
+++ 누구나 쉽게 SQL/hr.sql
@@ -1,9 +1,5 @@
-SELECT emp_name, age
-       ,CASE WHEN age BETWEEN 0 AND 19 THEN '10대'
-             WHEN age BETWEEN 20 AND 29 THEN '20대'
-             WHEN age BETWEEN 30 AND 39 THEN '30대'
-             WHEN age BETWEEN 40 AND 49 THEN '40대'
-             WHEN age BETWEEN 50 AND 59 THEN '50대'
-             ELSE '60대 이상'
-        END ages
-FROM EMP03;
+SELECT station_name
+    FROM subway_statistics
+WHERE gubun = '승차'
+GROUP BY station_name
+ORDER BY station_name;
