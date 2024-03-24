4f7df05b66cdf9d2943f00f6fbc8319b5f7e4df5
DainJung <wjdekdls3693@gmail.com>
2024-03-24 18:45:34 +0900

    DISTINCT 사용

--- 누구나 쉽게 SQL/hr.sql
+++ 누구나 쉽게 SQL/hr.sql
@@ -1,10 +1,4 @@
-SELECT station_name
-    ,boarding_time
-    ,gubun
-    ,MIN(passenger_number) min_value
-    ,MAX(passenger_number) max_value
-    ,SUM(passenger_number) sum_value
+SELECT DISTINCT station_name
   FROM subway_statistics
-GROUP BY station_name, boarding_time, gubun
-HAVING SUM(passenger_number) BETWEEN 15000 AND 16000
-ORDER BY 6 DESC;
+WHERE gubun = '승차'
+ORDER BY 1;
