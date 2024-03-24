0ad141f4f6c01628be9b5b5633dcb16ee6e74e61
DainJung <wjdekdls3693@gmail.com>
2024-03-24 14:16:11 +0900

    집계함수_지하철역별 승차 인원 통계

--- 누구나 쉽게 SQL/hr.sql
+++ 누구나 쉽게 SQL/hr.sql
@@ -1,6 +1,10 @@
-SELECT COUNT(*) cnt
+SELECT station_name
+    ,COUNT(*) cnt
     ,MIN(passenger_number) min_value
     ,MAX(passenger_number) max_value
     ,SUM(passenger_number) sum_value
     ,AVG(passenger_number) avg_value
-FROM subway_statistics;
+FROM subway_statistics
+WHERE gubun = '승차'
+GROUP BY station_name
+ORDER BY station_name;
