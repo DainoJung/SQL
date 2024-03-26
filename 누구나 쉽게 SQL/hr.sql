ef76d11e8971e6eea4e89c3bba476e6c8d66baa0
DainJung <wjdekdls3693@gmail.com>
2024-03-26 22:33:53 +0900

    카티션 곱

--- 누구나 쉽게 SQL/hr.sql
+++ 누구나 쉽게 SQL/hr.sql
@@ -1,6 +1,5 @@
 SELECT a.emp_id, a.emp_name, a.gender, a.age, a.dept_id,
        b.dept_id, b.dept_name, b.use_yn
   FROM emp_master a
-  FULL OUTER JOIN dept_master b
-    ON a.dept_id = b.dept_id
+      ,dept_master b
  ORDER BY a.emp_id;
