06d782c44f6214a555a3d116e71eba5498975af7
DainJung <wjdekdls3693@gmail.com>
2024-03-26 22:03:05 +0900

    ANSI 내부 조인

--- 누구나 쉽게 SQL/hr.sql
+++ 누구나 쉽게 SQL/hr.sql
@@ -1,6 +1,6 @@
 SELECT a.emp_id, a.emp_name, a.gender, a.age, a.dept_id,
        b.dept_id, b.dept_name, b.use_yn
   FROM emp_master a
-      ,dept_master b
- WHERE a.dept_id = b.dept_id
+INNER  JOIN dept_master b
+    ON a.dept_id = b.dept_id
  ORDER BY a.emp_id;
