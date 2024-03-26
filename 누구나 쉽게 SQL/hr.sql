fb5f954eefd80437c00941165f1cc7c513c3e6f5
DainJung <wjdekdls3693@gmail.com>
2024-03-26 22:27:43 +0900

    FULL OUTER JOIN

--- 누구나 쉽게 SQL/hr.sql
+++ 누구나 쉽게 SQL/hr.sql
@@ -1,7 +1,6 @@
 SELECT a.emp_id, a.emp_name, a.gender, a.age, a.dept_id,
        b.dept_id, b.dept_name, b.use_yn
   FROM emp_master a
-INNER  JOIN dept_master b
+  FULL OUTER JOIN dept_master b
     ON a.dept_id = b.dept_id
-WHERE a.gender = '남성'
  ORDER BY a.emp_id;
