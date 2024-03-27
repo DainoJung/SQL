4cb94edd394c29ab475014fbcefeb9e22388588e
DainJung <wjdekdls3693@gmail.com>
2024-03-27 10:29:44 +0900

    스칼라 서브쿼리

--- 누구나 쉽게 SQL/hr.sql
+++ 누구나 쉽게 SQL/hr.sql
@@ -1,5 +1,9 @@
-SELECT a.emp_id, a.emp_name, a.gender, a.age, a.dept_id,
-       b.dept_id, b.dept_name, b.use_yn
-  FROM emp_master a
-      ,dept_master b
- ORDER BY a.emp_id;
+SELECT a.emp_id
+      ,a.emp_name
+      ,a.gender
+      ,a.age
+      ,a.dept_id
+      ,( SELECT b.dept_name
+           FROM dept_master b
+          WHERE a.dept_id = b.dept_id ) dept_name
+FROM emp_master a;
