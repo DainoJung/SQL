9da05304e750458ba810980a5071869d35f1a6ba
DainJung <wjdekdls3693@gmail.com>
2024-03-28 15:57:00 +0900

    CREATE TABLE AS

--- 누구나 쉽게 SQL/hr.sql
+++ 누구나 쉽게 SQL/hr.sql
@@ -1,6 +1,7 @@
+CREATE TABLE subway_dml_test AS
 SELECT *
-  FROM dept_master a      
- WHERE a.dept_id NOT IN ( SELECT b.dept_id
-                            FROM emp_master b
-                           WHERE b.age BETWEEN 40 AND 49
-                        );	
+    FROM subway_statistics
+WHERE ROWNUM < 1;
+
+ALTER TABLE subway_dml_test
+ADD PRIMARY KEY ( seq_id );
